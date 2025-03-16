8626e8b7e4f632d138e532a531912a9123c45e4c
Brevillo <90069242+Brevillo@users.noreply.github.com>
2025-03-15 23:42:36 -0700

    added interpolation to brush tool

--- Assets/Tile Editor/Toolbar Actions/BrushToolAction.cs
+++ Assets/Tile Editor/Toolbar Actions/BrushToolAction.cs
@@ -8,6 +8,7 @@ public class BrushToolAction : ToolbarAction
     private List<Vector2Int> brushedTiles = new();
 
     private bool inverseBrushMode;
+    private Vector2Int previousMouseCell;
 
     protected virtual GameTile DrawingTile => inverseBrushMode
         ? null
@@ -23,12 +24,23 @@ public class BrushToolAction : ToolbarAction
         brushedTiles.Clear();
         
         inverseBrushMode = EditorState.GetTile(SpaceUtility.MouseCell).gameTile == EditorState.PrimaryTile;
+
+        previousMouseCell = SpaceUtility.MouseCell;
     }
 
     protected override void OnPressed()
     {
-        TilePlacer.PlaceTile(SpaceUtility.MouseCell, new(DrawingTile));
-        brushedTiles.Add(SpaceUtility.MouseCell);
+        Vector2Int current = SpaceUtility.MouseCell;
+        
+        var positions = GetPointsOnLine(previousMouseCell, current);
+
+        var tiles = new TileData[positions.Length];
+        Array.Fill(tiles, new(DrawingTile));
+        
+        TilePlacer.PlaceTiles(positions, tiles);
+        brushedTiles.AddRange(positions);
+        
+        previousMouseCell = current;
     }
 
     protected override void OnReleased()
@@ -51,5 +63,42 @@ public class BrushToolAction : ToolbarAction
         EditorState.SetTiles(brushedTiles.ToArray(), tiles, changelogMessage);
         
         brushedTiles.Clear();
+    }
+
+    private static Vector2Int[] GetPointsOnLine(Vector2Int start, Vector2Int end)
+    {
+        var result = new List<Vector2Int>();
+
+        Vector2Int delta = new(
+            Mathf.Abs(end.x - start.x),
+            -Mathf.Abs(end.y - start.y));
+        
+        Vector2Int sign = new(
+            (int)Mathf.Sign(end.x - start.x),
+            (int)Mathf.Sign(end.y - start.y));
+        
+        int error = delta.x + delta.y;
+
+        while (true)
+        {
+            result.Add(start);
+            int e2 = 2 * error;
+
+            if (e2 >= delta.y)
+            {
+                if (start.x == end.x) break;
+                error += delta.y;
+                start.x += sign.x;
+            }
+            
+            if (e2 <= delta.x)
+            {
+                if (start.y == end.y) break;
+                error += delta.x;
+                start.y += sign.y;
+            }
+        }
+        
+        return result.ToArray();
     }
 }
