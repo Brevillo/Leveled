98d2bf23ccb9f217f92ff12f6d7d74c59f1b2bea
Brevillo <90069242+Brevillo@users.noreply.github.com>
2025-12-17 23:23:23 -0800

    implemented selection size change sound

--- Assets/Design Space/Level Editor/Toolbar Actions/ToolbarActionsUI.cs
+++ Assets/Design Space/Level Editor/Toolbar Actions/ToolbarActionsUI.cs
@@ -6,14 +6,25 @@ public class ToolbarActionsUI : MonoBehaviour
     [SerializeField] private SelectionUIElement selectionOutline;
     [SerializeField] private SelectionUIElement hoverSelection;
     [SerializeField] private SelectionUIElement tilemapBoundsOutline;
+    [SerializeField] private SoundEffect selectionSizeChangeSound;
     [Header("References")]
     [SerializeField] private TileEditorState editorState;
     [SerializeField] private GameStateManager gameStateManager;
     [SerializeField] private ToolbarBlackboard blackboard;
     [SerializeField] private TilePlacer tilePlacer;
+
+    private Vector3Int previousSelectionSize;
     
     private void LateUpdate()
     {
+        var newSelectionSize = blackboard.hoverSelection.size;
+        if (newSelectionSize != previousSelectionSize)
+        {
+            selectionSizeChangeSound.Play();
+        }
+
+        previousSelectionSize = newSelectionSize;
+        
         selectionOutline.SetSize(blackboard.selection);
         selectionOutline.gameObject.SetActive(blackboard.selection != default && gameStateManager.GameState == GameState.Editing);
 
