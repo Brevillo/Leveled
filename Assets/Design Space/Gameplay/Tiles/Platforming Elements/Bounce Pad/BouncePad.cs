f91657993ada1aca6a2ea02b854f047acc0c38c5
Aidan Guthrie <rampagea03@gmail.com>
2025-12-25 19:12:44 -0800

    BouncePad Bounce

--- Assets/Design Space/Gameplay/Tiles/Platforming Elements/Bounce Pad/BouncePad.cs
+++ Assets/Design Space/Gameplay/Tiles/Platforming Elements/Bounce Pad/BouncePad.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Events;
 
 public class BouncePad : MonoBehaviour
 {
@@ -7,12 +9,14 @@ public class BouncePad : MonoBehaviour
     [SerializeField] private Vector2 direction;
     [SerializeField] private BounceSnapAxis snapAxis;
     [SerializeField] private Transform bouncePosition;
+    [SerializeField] private UnityEvent bounceSound;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out Bounceable bounceable))
         {
             bounceable.Bounce(new(bouncePosition.position, snapAxis, direction.normalized * force));
+            bounceSound.Invoke();
         }
     }
 }
