6e6bd06e4b8857bb26f0d62baba8dcc25ee79d2c
Brevillo <90069242+Brevillo@users.noreply.github.com>
2025-12-31 14:50:59 -0800

    asteroid rework

--- Assets/Design Space/Gameplay/Tiles/Enemies/Asteroid/Asteroid.cs
+++ Assets/Design Space/Gameplay/Tiles/Enemies/Asteroid/Asteroid.cs
@@ -1,59 +1,42 @@
+using System;
 using System.Linq;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    [SerializeField] private float rotationDuration;
-    [SerializeField] private float rotationRadius;
-    [SerializeField] private float rotationAnchor;
-    [SerializeField] private int rotationDirection;
-    [SerializeField] private Transform projectilePivot;
-    [SerializeField] private float hitboxDuration;
-    [SerializeField] private GameObject hitbox;
-    [SerializeField] private LineRenderer frontLine;
-    [SerializeField] private LineRenderer backLine;
-    [SerializeField] private float lineAngleRange;
-    [SerializeField] private int pointsPerLine;
-    [SerializeField] private SpriteRenderer sprite;
-    [SerializeField] private string frontLayer;
-    [SerializeField] private string backLayer;
-    [SerializeField] private AnimationCurve sizeOverRangeCurve;
+    [SerializeField] private float gravity;
+    [SerializeField] private new Rigidbody2D rigidbody;
+    [SerializeField] private TilePlacerReference tilePlacerReference;
+    [SerializeField] private float bottomOffset;
+    [SerializeField] private float bottomDuration;
     
-    private float rotationTimer;
-
+    private Vector2 startingPosition;
+    private float bottomTimer;
+    
     private void Start()
     {
-        frontLine.positionCount = pointsPerLine;
-        frontLine.SetPositions(GetPoints(-lineAngleRange / 2f, 0f));
-        
-        backLine.positionCount = pointsPerLine;
-        backLine.SetPositions(GetPoints(0f, lineAngleRange / 2f));
-
-        Vector3[] GetPoints(float start, float end) => Enumerable.Range(0, pointsPerLine)
-            .Select(i =>
-            {
-                float rads = Mathf.Lerp(start, end, (float)i / (pointsPerLine - 1)) * Mathf.Deg2Rad;
-                return new Vector3(0f, Mathf.Cos(rads) - 1f, Mathf.Sin(rads)) * rotationRadius;
-            })
-            .ToArray();
+        startingPosition = rigidbody.position;
     }
 
     private void Update()
     {
-        rotationTimer = (rotationTimer + Time.deltaTime) % rotationDuration;
+        rigidbody.linearVelocity += Vector2.down * (gravity * Time.deltaTime);
 
-        hitbox.SetActive(rotationTimer < hitboxDuration);
+        float bottom = tilePlacerReference.value.Bounds.min.y - bottomOffset;
+        
+        if (rigidbody.position.y < bottom)
+        {
+            bottomTimer += Time.deltaTime;
+            rigidbody.position = new Vector2(startingPosition.x, bottom);
 
-        float rotationPercent = rotationTimer / rotationDuration;
-
-        sprite.sortingLayerName = (rotationPercent - 0.5f) * rotationDirection > 0f
-            ? frontLayer
-            : backLayer;
-
-        float sizePercent = 1f - Mathf.InverseLerp(0f, lineAngleRange / 360f, 1f - Mathf.Abs(1f - 2f * rotationPercent));
-        projectilePivot.localScale = Vector3.one * sizeOverRangeCurve.Evaluate(sizePercent);
-
-        float rads = (rotationAnchor + rotationPercent * 360f * rotationDirection) * Mathf.Deg2Rad;
-        projectilePivot.localPosition = new Vector3(0f, Mathf.Cos(rads) - 1f, Mathf.Sin(rads)) * rotationRadius;
+            if (bottomTimer > bottomDuration)
+            {
+                rigidbody.linearVelocity = Vector2.up * Mathf.Sqrt(2f * gravity * (startingPosition.y - bottom));
+            }
+        }
+        else
+        {
+            bottomTimer = 0f;
+        }
     }
 }
